3dfd64d3ccd0967c0bbc71436b5d88fd0881582c
Valentin Golev <me@va1en0k.net>
2013-01-28 05:28:19 +0400

    it moves

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [jayq "2.0.0"]
-                          [hiccups "0.2.0"]]
+                           [hiccups "0.2.0"]
+                           [org.clojure/math.combinatorics "0.0.3"]]
 					  :plugins [[lein-cljsbuild "0.3.0"]] ; cljsbuild plugin
             :cljsbuild {
              :builds [{
--- src-cljs/cletris/board.cljs
+++ src-cljs/cletris/board.cljs
@@ -7,16 +7,18 @@
         [jayq.util :only [log]]))
 
 
+(defn cartesian-product [xs ys]
+  (for [x xs y ys] [x y]))
+
 (def $body ($ "body"))
 
 (defn html-into [$parent content]
   (append-to ($ (hiccups/html content)) $parent))
 
-(.click (html-into $body [:a {:href "#repl"} "REPL"])
-  #(repl/connect "http://localhost:9000/repl"))
+; (.click (html-into $body [:a {:href "#repl"} "REPL"])
+;   #(repl/connect "http://localhost:9000/repl"))
 
 (def $content (html-into $body [:div#content]))
-
 
 (defhtml board-template [board]
   [:table.board
@@ -26,64 +28,120 @@
          [:td {:class (case cell
                         2 :figur
                         1 :filld
-                        0 :empty)}])])])
+                        0 :empty)} "."])])])
 
 
+(def board-initial [[0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [1 0 0 0 0 0]
+                    [1 1 0 0 0 1]])
 
-(defn transpose [board]
-  (vec (apply map vector board)))
-
-(defn cycle-1 [coll]
-  (conj (vec (rest coll)) (first coll)))
-
-
-
-(defn top-direction [direction board]
-  (case direction
-    :left (transpose board)
-    :right (reverse (transpose board))))
-
-(defn untop-direction [direction board]
-  (case direction
-    :left (transpose board)
-    :right (transpose (reverse board))))
-
-(defn move [board direction]
-  (let [topped (top-direction direction board)]
-    (if (every? #(= % :empty) (first topped))
-        (untop-direction direction (cycle-1 topped))
-        board)))
-
-
-(def board [[0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [0 0 0 0 0 0]
-            [1 0 0 0 0 0]
-            [1 1 0 0 0 1]])
-
+(defn change-2d [coll x y val]
+  (assoc coll x
+    (assoc (coll x) y
+      val)))
 
 (def figure [[1 0]
              [1 1]])
 
+(def figur-pos-initial [0 2])
+
+(defn get-2d [coll y x]
+  ((coll y) x))
+
+(defn set-2d [coll y x val]
+  (assoc coll y
+    (assoc (coll y) x
+      val)))
+
+(defn width [col] (count (col 0)))
+(defn height [col] (count col))
+
+(def board-width (width board-initial))
+
+(defn figure-on-board [board [fy fx] figure]
+  "Returns either merged board,
+   or :invalid in case of invalid position,
+   or :fail in case of game end"
+
+  (let [figure-width (count (figure 0))
+        figure-height (count figure)]
+
+    (if (or (> (+ fx (width figure)) board-width)
+            (> (+ fy (height figure)) (height board)))
+          :invalid
+
+        (reduce
+         (fn [brd [y x]]
+            (if (= brd :fail) :fail
+              (let [by (+ y fy)
+                    bx (+ fx x)
+                    fig-val (get-2d figure y x)
+                    brd-val (get-2d brd by bx)]
+                (case [fig-val brd-val]
+                  [1 1] :fail
+                  [1 0] (set-2d brd by bx 2)
+                  brd))))
+
+                board
+
+               (cartesian-product
+                (range (height figure))
+                (range (width figure)))))))
+
 
 (def keydowns (.keydownAsObservable cletris.board/$body))
 
-(def movekeys
+(def move-signal
   (.where
     (.select keydowns #(case (.-keyCode %) 37 :left, 39 :right, nil))
      (complement nil?)))
 
 
+(def pos-signal
+  (.startWith
+    (.scan move-signal
+         figur-pos-initial
+         (fn [[y x] move]
+            (let [new-x (case move
+                              :left (- x 1)
+                              :right (+ x 1))]
+              (if (or (< new-x 0) (> (+ (width figure) new-x) board-width)) [y x]
+                  [y new-x]))))
+    figur-pos-initial))
+
+(def board-signal
+  (.where
+      (.select pos-signal
+           ; board-initial
+           (fn [figure-pos]
+            (let [new-board (figure-on-board board-initial figure-pos figure)]
+              (if (= :invalid new-board) nil
+                  new-board))))
+      (complement nil?)))
 
 
-(.subscribe movekeys log)
+
+(.subscribe move-signal log)
+(.subscribe pos-signal #(log (str %)))
+(.subscribe board-signal log)
+
+
+
+(.subscribe board-signal #(.html $content (board-template %)))
+
+
+
+
+
 
 ; (.keydown $body (fn [e]
 ;    (when-let [key (key-codes (.-keyCode e))]
